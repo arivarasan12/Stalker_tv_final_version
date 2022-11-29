ad4e5e1f84baee20c7a4871239a87e2e0f42bd92
arivarasan12 <89193472+arivarasan12@users.noreply.github.com>
2022-11-29 17:17:36 +0530

    Add files via upload

--- play.php
+++ play.php
@@ -4,11 +4,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-<link rel="shortcut icon" type="image/x-icon" href="https://play-lh.googleusercontent.com/A0UZO-jMqgtkSZ29w8xUbU0g55JLBa65zZX3QpFUB9sOjz9a7kLcb3do2Sg-pHIH4w0=w240-h480">
+<link rel="shortcut icon" type="image/x-icon" href="logo.jpg">
 <script type="text/javascript" src="https://ajax.cloudflare.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 <script src="https://cdn.plyr.io/2.0.18/plyr.js"></script>
-<link rel="stylesheet" href="http://localhost/plyr.css">
+<link rel="stylesheet" href="/plyr.css">
 <style>
 html {
   font-family: Poppins;
@@ -147,17 +147,17 @@ function videovisible() {
 
 var url="<?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://live.new4k.tv/stalker_portal/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml');
+curl_setopt($ch, CURLOPT_URL, 'http://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3',
     'X-User-Agent: Model: MAG250; Link: WiFi',
-    'Referer: http://live.new4k.tv/stalker_portal/c/',
+    'Referer: http://tv.tvzon.tv/stalker_portal/c/',
     'Accept: /',
-    'Host: live.new4k.tv',
+    'Host: tv.tvzon.tv',
     'Connection: Keep-Alive'));
-curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1a:79:d6:82:fc; stb_lang=en; timezone=GMT');
+curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1A:79:15:CB:57; stb_lang=en; timezone=GMT');
 
 $response = curl_exec($ch);
 $decoded_json = json_decode($response, true);
@@ -168,19 +168,19 @@ $randome=$customers['random'];
 curl_close($ch);
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://live.new4k.tv/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=C17992B38C6B1&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=c17992b38c6b1ef0e854c478029b8ab3&timestamp=1666586897&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AD6%3A82%3AFC%22%2C%22sn%22%3A%22C17992B38C6B1%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22395d1941f5b3f4649eb51e9cc13e05ae%22%7D&JsHttpRequest=1-xml');
+curl_setopt($ch, CURLOPT_URL, 'http://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=C17992B38C6B1&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=c17992b38c6b1ef0e854c478029b8ab3&timestamp=1666586897&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AD6%3A82%3AFC%22%2C%22sn%22%3A%22C17992B38C6B1%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22395d1941f5b3f4649eb51e9cc13e05ae%22%7D&JsHttpRequest=1-xml');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3',
     'X-User-Agent: Model: MAG250; Link: WiFi',
-    'Referer: http://live.new4k.tv/stalker_portal/c/',
+    'Referer: http://tv.tvzon.tv/stalker_portal/c/',
     'Authorization: Bearer '.$token,
     'Accept: /',
-    'Host: live.new4k.tv',
+    'Host: tv.tvzon.tv',
     'Connection: Keep-Alive',
 ]);
-curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1a:79:d6:82:fc; stb_lang=en; timezone=GMT');
+curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1A:79:15:CB:57; stb_lang=en; timezone=GMT');
 
 $response = curl_exec($ch);
 
@@ -190,34 +190,35 @@ curl_close($ch);
 
 $ch = curl_init();
 $channelno=$_REQUEST["q"];
-curl_setopt($ch, CURLOPT_URL,'http://live.new4k.tv/stalker_portal/server/load.php?type=itv&action=create_link&forced_storage=undefined&download=0&cmd=ffrt+http%3A%2F%2Flocalhost%2Fch%2F'.$channelno);
+curl_setopt($ch, CURLOPT_URL,'http://tv.tvzon.tv/stalker_portal/server/load.php?type=itv&action=create_link&forced_storage=undefined&download=0&cmd=ffrt+http%3A%2F%2Flocalhost%2Fch%2F'.$channelno);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3',
     'X-User-Agent:  Model: MAG250; Link: WiFi',
-    'Referer:  http://live.new4k.tv/stalker_portal/c/',
+    'Referer:  http://tv.tvzon.tv/stalker_portal/c/',
     'Authorization: Bearer '.$token,
     'Accept:  /',
-    'Host:  live.new4k.tv',
+    'Host:  tv.tvzon.tv',
     'Connection:  Keep-Alive',
     
-    // 'Cookie: mac=00:1a:79:d6:82:fc; stb_lang=en; timezone=GMT'
+    // 'Cookie: mac=00:1A:79:15:CB:57; stb_lang=en; timezone=GMT'
 ));
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1a:79:d6:82:fc; stb_lang=en; timezone=GMT');
+curl_setopt($ch, CURLOPT_COOKIE, 'mac=00:1A:79:15:CB:57; stb_lang=en; timezone=GMT');
 $response = curl_exec($ch);
 $decoded_json = json_decode($response, true);
- 
+//  echo $response;
 $customers = $decoded_json['js'];
 $channelurl= $customers['cmd'];
-echo $channelurl.'"';
+$channelurl1=str_replace("http://hin2.error-404.website","http://195.181.169.66",$channelurl);
+echo $channelurl1.'"';
 curl_close($ch);
 ?>;
 plyr.setup(video);
 
- if(Hls.isSupported()) {
+ if(Hls.notSupported()) {
     var video = document.getElementById('video');
     var hls = new Hls();
     hls.loadSource(url);
